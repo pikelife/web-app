0a59146a979935691672b293c1ce5753ba1f6f48
idriss said alaoui <idriss.said.alaoui@pikelife.com>
2015-08-26 00:28:57 -0400

    put a model of Post and GEt of mongo db

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ var express = require('express'),
     httpRequest = require('request'),
     mongoose = require('mongoose');
 
-var joinOnApp = this;
+var _this = this;
 
 var profileSchema;
-joinOnApp.profileSchema = function(val){
+_this.profileSchema = function(val){
   if(val !== undefined) profileSchema = val;
   return profileSchema;
 };
 
 var profileModel;
-joinOnApp.profileModel = function(val){
+_this.profileModel = function(val){
   if(val !== undefined) profileModel = val;
   return profileModel;
 };
@@ -31,20 +31,17 @@ db.once('open', function (callback) {
     name : String,
     password : String
   });
-  joinOnApp.profileSchema(profileSchema);
+  _this.profileSchema(profileSchema);
   
   var Profile = mongoose.model('Profile', profileSchema);
-  joinOnApp.profileModel(Profile);
+  _this.profileModel(Profile);
   
 });
 
-var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://neo4j:bartra56390@198.100.145.160:7474');
-
 var app = express();
 
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-app.use('/assets', express.static(__dirname + '/assets'));
+//app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+//app.use('/assets', express.static(__dirname + '/assets'));
 
 
 app.use(bodyParser.urlencoded({
@@ -60,48 +57,37 @@ function getQueryObj(url){
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-
-app.get('/order', function (req, res) {
-  res.sendFile(__dirname + '/order.html');
-});
-
-app.route('/demo')
-  .get(function(req, res) {
-    db.cypher({
-    query: 'MATCH (n) WHERE has(n.name) RETURN DISTINCT "node" as element, n.name AS name LIMIT 25 UNION ALL MATCH ()-[r]-() WHERE has(r.name) RETURN DISTINCT "relationship" AS element, r.name AS name LIMIT 25',
-    }, function (err, results) {
-      if (err) throw err;
-      res.send(results);
-      if (!result) {
-        console.log('No user found.');
-      } else {
-        var user = result['u'];
-        console.log(JSON.stringify(user, null, 4));
-      }
-    });
-  });
   
 app.route('/profile')
   .get(function(req, res) {
-    // to be done
+    var query = getQueryObj(req.url);
+    console.dir("GET received query " + JSON.stringify(query.email));
+    var queryDb = _this.profileModel().findOne({ 'email': query.email});
+    queryDb.exec(function (err, obj) {
+      if (err) return console.error(err);
+      console.log("POST profile : " + obj);
+      res.send(obj); 
+    });
   })
   .post(function(req, res) {
     var query = getQueryObj(req.url);
     if(query.isRegister === 'true'){
-      var newProfile = new joinOnApp.profileModel()({  "email" : req.body.email,
+      console.log("received query " + req.body);
+      var newProfile = new _this.profileModel()({  "email" : req.body.email,
                                                        "name" : req.body.name,
                                                        "password" : req.body.password });
       newProfile.save(function (err, obj) {
         if (err) return console.error(err);
+        console.log("GET after existing user profile : " + obj);
         res.send(obj);
       });
     }else{
       console.log(req.body);
-      var queryDb = joinOnApp.profileModel().findOne({ 'email': req.body.email,
+      var queryDb = _this.profileModel().findOne({ 'email': req.body.email,
                                                        'password' : req.body.password});
       queryDb.exec(function (err, obj) {
         if (err) return console.error(err);
-        console.log(obj);
+        console.log("POST profile : " + obj);
         res.send(obj);
       });
     }
