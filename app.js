568027fc5852c90a24559470c9572dddf7e89635
idriss said alaoui <idriss.said.alaoui@pikelife.com>
2015-10-06 00:37:57 -0400

    fixed some stuff

--- app.js
+++ app.js
@@ -77,9 +77,9 @@ app.use(function(req, res, next) {
   // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', '*');
 
     // Pass to next layer of middleware
     next();
