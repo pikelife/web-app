f3422826660f551529750a5c05ef4461d979eaa4
idriss said alaoui <idriss.said.alaoui@pikelife.com>
2015-10-06 21:41:34 -0400

    npm cors included

--- app.js
+++ app.js
@@ -73,11 +73,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 
 function getQueryObj(url){
